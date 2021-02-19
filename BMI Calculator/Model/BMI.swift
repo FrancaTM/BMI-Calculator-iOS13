5a2bf0dd6e5db46a44caf1419191b863b1df89bd
FrancaTM <francatm@outlook.com>
2021-02-19 12:53:28 -0300

    finish project add BMI struct use optionals

---
+++ BMI Calculator/Model/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Tulio Marcos Franca on 19/02/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
